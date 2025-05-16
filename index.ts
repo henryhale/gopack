3916579291a3aeadb1e0a350ee6d536cf0a37da9
Henry Hale <92443116+henryhale@users.noreply.github.com>
2025-05-16 23:26:05 +0300

    fix: update version retrieval

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import fs from "node:fs";
 import fsp from "node:fs/promises";
 import path from "node:path";
 
-const binaries = [
+const BINARIES = [
     "linux-arm",
     "linux-arm64",
     "linux-amd64",
@@ -13,6 +13,43 @@ const binaries = [
     "windows-arm64",
     "windows-amd64",
 ];
+
+const GOARM_VERSION = 7;
+
+const DEFAULT_VERSION = "v0.0.0";
+
+async function execAndExtractOutput(cmd: string) {
+    let result: string = "";
+
+    await exec(cmd, [], {
+        listeners: {
+            stdout(data) {
+                result += data.toString().trim();
+            },
+        },
+    });
+
+    return result;
+}
+
+async function getLatestTagOrCommit() {
+    let result: string = DEFAULT_VERSION;
+    try {
+        // get latest tag
+        result = await execAndExtractOutput(
+            `git describe --tags --always --abbrev=0`,
+        );
+        return result;
+    } catch {
+        // fallback on latest commit hash
+        try {
+            result = await execAndExtractOutput(`git rev-parse --short HEAD`);
+            return result;
+        } finally {
+            return result;
+        }
+    }
+}
 
 async function build() {
     try {
@@ -36,12 +73,12 @@ async function build() {
         }
 
         // get project version
-        const projectVersion = await exec("git describe --tags --always");
+        const projectVersion = await getLatestTagOrCommit();
 
         // start building binaries
-        for (const bin of binaries) {
+        for (const bin of BINARIES) {
             const [goos, goarch] = bin.split("-");
-            const goarm = goarch === "arm" ? "GOARM=7" : "";
+            const goarm = goarch === "arm" ? `GOARM=${GOARM_VERSION}` : "";
             const isWindows = goos === "windows";
 
             const binName = `${projectName}-${projectVersion}-${goos}-${goarch}`;
